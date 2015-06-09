fde085c78b47d3fea0a896664e12fc3ecff477b3
Jani <Janijesenovec@phantomsmedia.com>
2015-06-08 20:51:00 -0700

    WorkonMis + Updt WL

--- SL_CO20_ByDustWeRide.takistan/f/config.hpp
+++ SL_CO20_ByDustWeRide.takistan/f/config.hpp
@@ -40,6 +40,7 @@ class F // Defines the "owner"
 	    class assignGearMan {};
 	    class assignGearVehicle {};
     };
+	/*
 	class missionConditions
 	{
 		file = "f\missionConditions";
@@ -47,6 +48,7 @@ class F // Defines the "owner"
 		class SetFog{};
 		class SetWeather{};
 	};
+	*/
 	/*
 	class cache
 	{
